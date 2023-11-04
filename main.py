32c0efa37b38002e8e8fdd6a039728ba03d57999
krizhnaa <krizhnaprem@gmail.com>
2023-11-04 22:19:42 +0530

    end of the day commit...

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+manu = Menu()
+
+cm_continue = True
+
+while cm_continue:
+    user_input = input(f'What would you like? ( {manu.get_items()} ) : ')
+    if user_input == 
