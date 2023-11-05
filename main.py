39e612fef37cd5332d3bc5e9ea371a18b47f94bb
krizhnaa <krizhnaprem@gmail.com>
2023-11-05 08:58:34 +0530

    End of first session

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from money_machine import MoneyMachine
 
 manu = Menu()
 cm = CoffeeMaker()
+mm = MoneyMachine()
+# mi = MenuItem()
 
 cm_continue = True
 
@@ -11,12 +13,17 @@ while cm_continue:
     user_input = input(f'What would you like? ( {manu.get_items()} ) : ')
     if user_input == 'off':
         cm_continue = False
-
-    if user_input == 'report':
-        print(cm.report())
+    elif user_input == 'report':
+        cm.report()
+        mm.report()
         user_input = input(f'What would you like? ( {manu.get_items()} ) : ')
 
-    if cm.is_resource_sufficient(manu.find_drink(user_input)) == True:
-        print('can make')
-    else:
-        print('Sorry')
+
+    can_mk = cm.is_resource_sufficient(manu.find_drink(user_input))
+
+    # if can_mk == True:
+    #     mm.make_payment()
+    # print(mi.cost)
+
+
+
