ba938784ed54b44074ce7bdb893be360243825e4
krizhnaa <krizhnaprem@gmail.com>
2023-11-05 08:28:02 +0530

    Day - 2, first commit

--- main.py
+++ main.py
@@ -3,9 +3,20 @@ from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
 manu = Menu()
+cm = CoffeeMaker()
 
 cm_continue = True
 
 while cm_continue:
     user_input = input(f'What would you like? ( {manu.get_items()} ) : ')
-    if user_input == 
+    if user_input == 'off':
+        cm_continue = False
+
+    if user_input == 'report':
+        print(cm.report())
+        user_input = input(f'What would you like? ( {manu.get_items()} ) : ')
+
+    if cm.is_resource_sufficient(manu.find_drink(user_input)) == True:
+        print('can make')
+    else:
+        print('Sorry')
